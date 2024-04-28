dca0b481058c4c9de5e1811990b92ab9277bc4a9
Nuzhyn Ivan <nuzhin.ivan@gmail.com>
2024-04-28 20:06:26 +0200

    Replaced kivy.config.ConfigParser with default python configparser

--- Models/utils/ConfigParser.py
+++ Models/utils/ConfigParser.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 
-from kivy.config import ConfigParser
+import configparser
 
 
 DEFAULT_SECTION = "DEFAULT"
 MAIN_CONFIG_PATH = "Models/main.ini"
 BACKUP_CONFIG_PATH = "../TODO_config.ini"
 
-_configfile = ConfigParser(interpolation=None)
-_configfile.read(MAIN_CONFIG_PATH)
+_config_parser = configparser.ConfigParser(interpolation=None)
+_config_parser.read(MAIN_CONFIG_PATH)
 
 
 def get_option_value(option: str, default: str = None) -> str:
@@ -23,7 +23,7 @@ def get_option_value(option: str, default: str = None) -> str:
     :raises:
     """
     try:
-        return _configfile.get(DEFAULT_SECTION, option)
+        return _config_parser.get(DEFAULT_SECTION, option)
     except:
         return default
 
@@ -38,8 +38,10 @@ def set_option_value(option: str, value: str):
     :rtype:
     :raises:
     """
-    _configfile.set(DEFAULT_SECTION, option, value)
-    _configfile.write()
+    _config_parser.set(DEFAULT_SECTION, option, value)
+
+    with open(MAIN_CONFIG_PATH, "w") as configfile:
+        _config_parser.write(configfile)
     shutil.copyfile(MAIN_CONFIG_PATH, BACKUP_CONFIG_PATH)
 
 
@@ -47,6 +49,8 @@ def load_config():
     # TODO: move out str to var, like '../TODO_config.ini'
     if os.path.exists(BACKUP_CONFIG_PATH):
         shutil.copyfile(BACKUP_CONFIG_PATH, MAIN_CONFIG_PATH)  # load saved config
-        _configfile.read(MAIN_CONFIG_PATH)  # refresh Kivy config
-        _configfile.write()  # save config to main.ini file # TODO - is this step needed?
-        shutil.copyfile(MAIN_CONFIG_PATH, BACKUP_CONFIG_PATH)
+
+    _config_parser = configparser.ConfigParser(interpolation=None)
+    _config_parser.read(MAIN_CONFIG_PATH)
+    # save config to main.ini file # TODO - is this step needed?
+    shutil.copyfile(MAIN_CONFIG_PATH, BACKUP_CONFIG_PATH)
